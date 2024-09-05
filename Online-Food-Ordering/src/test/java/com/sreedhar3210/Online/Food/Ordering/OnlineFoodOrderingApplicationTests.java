bfed0dd2445f53c6c1572fc476fab0bd66b10656
sreedhar3210 <sreedhar.palampalli@gmail.com>
2024-09-05 18:27:50 +0530

    initial setup

---
+++ Online-Food-Ordering/src/test/java/com/sreedhar3210/Online/Food/Ordering/OnlineFoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreedhar3210.Online.Food.Ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
